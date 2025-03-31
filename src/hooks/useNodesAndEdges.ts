3e234e4174d55cb481764abd377dbb1244798744
Pavel <samasyt@mail.ru>
2025-03-31 12:20:57 +0300

    fix: deleted unuseable variables

--- src/hooks/useNodesAndEdges.ts
+++ src/hooks/useNodesAndEdges.ts
@@ -4,8 +4,6 @@ import { ref, type Ref } from "vue";
 export default function useNodesAndEdges(newjsonFromTextArea) {
   const objectNodes: Ref<Nodes> = ref({});
   const objectEdges: Ref<Edges> = ref({});
-  const obj1: Ref<Nodes> = ref({});
-  const obj2: Ref<Edges> = ref({});
 
   function processNodes(nodes, obj1) {
     for (let itemOfElement of Object.keys(nodes)) {
@@ -39,10 +37,7 @@ export default function useNodesAndEdges(newjsonFromTextArea) {
     });
   }
 
-  processJson(newjsonFromTextArea, obj1, obj2);
-
-  objectNodes.value = obj1.value;
-  objectEdges.value = obj2.value;
+  processJson(newjsonFromTextArea, objectNodes, objectEdges);
 
   return { objectNodes, objectEdges };
 }
